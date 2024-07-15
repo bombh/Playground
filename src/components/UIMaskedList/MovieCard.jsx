ac1b0d49c021ec17b74f590f00789dc88733446f
bombh <fabrice@bxxl.be>
2024-07-15 04:13:10 +0200

    Rating comp

--- src/components/UIMaskedList/MovieCard.jsx
+++ src/components/UIMaskedList/MovieCard.jsx
@@ -1,11 +1,15 @@
 import { View, Text, Image, Dimensions, Animated } from "react-native"
 import React from "react"
+import Rating from "../movie/Rating"
+import { styled } from "nativewind"
 
 const { width, height } = Dimensions.get("window")
 const ITEM_SIZE = width * 0.7
 const OUTER_SIZE = (width - ITEM_SIZE) / 2
 
-const MovieCard = ({ index, title, thumbnail, director, year, plot, _scrollX }) => {
+const SRating = styled(Rating)
+
+const MovieCard = ({ index, title, thumbnail, director, year, imdbRating, plot, _scrollX }) => {
    // If dummy outer items, return a view
    if (!title) return <View style={{ width: OUTER_SIZE, height: ITEM_SIZE }} />
 
@@ -33,16 +37,17 @@ const MovieCard = ({ index, title, thumbnail, director, year, plot, _scrollX }) 
             <Text
                style={{ maxWidth: ITEM_SIZE * 0.8 }}
                numberOfLines={1}
-               className="text-stone-800 text-lg font-semibold text-center px-2"
+               className="text-stone-800 text-lg font-bold text-center px-2"
             >
                {title}
             </Text>
-            <Text className="text-stone-800 text-sm text-center">
+            <Text className="text-stone-500 text-sm text-center px-2 pb-1">
                {director} ({year})
             </Text>
+            <Rating rating={imdbRating} />
             <Text
                style={{ maxWidth: ITEM_SIZE * 0.8 }}
-               className="text-stone-800 text-xs text-justify mt-2"
+               className="text-stone-800 text-xs text-justify px-2 mt-2"
                numberOfLines={5}
             >
                {plot}
--- src/components/movie/Rating.jsx
+++ src/components/movie/Rating.jsx
@@ -1,10 +1,37 @@
 import { View, Text } from "react-native"
 import React from "react"
+import { StarIcon as StarOff } from "react-native-heroicons/outline"
+import { StarIcon as StarOn } from "react-native-heroicons/solid"
+import colors from "tailwindcss/colors"
 
-const Rating = () => {
+const STARS_MAX = 5
+
+const Rating = ({ rating }) => {
+   const filledStars = Math.round(rating / 2)
+   const emptyStars = STARS_MAX - filledStars
+   const stars = [...Array(filledStars).fill("on"), ...Array(emptyStars).fill("off")]
+
    return (
-      <View>
-         <Text>Rating</Text>
+      <View className="flex-row space-x-2">
+         <Text className="text-stone-500 text-xs font-bold">{rating}</Text>
+
+         <View className="flex-row space-x-0">
+            {stars.map((star, index) => {
+               return star === "on" ? (
+                  <StarOn
+                     key={index}
+                     size={16}
+                     color={colors.lime[400]}
+                  />
+               ) : (
+                  <StarOff
+                     key={index}
+                     size={16}
+                     color={colors.lime[400]}
+                  />
+               )
+            })}
+         </View>
       </View>
    )
 }
